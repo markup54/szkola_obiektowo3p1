94a68e6197504fcec42c40c5715fd3095d35a708
markup54 <marzenakupka@tlen.pl>
2024-09-18 14:34:26 +0200

    klasa Osoba

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Osoba osobaJas = new Osoba("Jaś","Kowalski",12);
+        System.out.println("Imie: "+osobaJas.imie);
+        osobaJas.imie = "Jan";
+        System.out.println("Imie nowe"+osobaJas.imie);
+
+    }
+}
