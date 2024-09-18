1901b422c54dde7f92da00eacbe9a677a5eed734
markup54 <marzenakupka@tlen.pl>
2024-09-18 15:35:00 +0200

    przeciazanie konstruktora dziedziczenia

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Osoba osobaJas = new Osoba("Jaś","Kowalski",12);
-        System.out.println("Imie: "+osobaJas.imie);
-        osobaJas.imie = "Jan";
-        System.out.println("Imie nowe"+osobaJas.imie);
+        System.out.println("Imie: "+osobaJas.getImie());
+        osobaJas.setImie("Jan");
+        System.out.println("Imie nowe"+osobaJas.getImie());
+        Osoba osobaIsia = new Osoba("Isia","Iksińska");
+        Osoba osbaX = new Osoba();
+        System.out.println(osobaIsia.getWiek());
+        System.out.println(osbaX.getWiek());
 
+        System.out.println(osobaIsia);
     }
 }
---
+++ src/Uczen.java
@@ -0,0 +1,17 @@
+public class Uczen extends Osoba{
+    /*
+    Uczen to klasa potomna
+    Osoba to klasa bazowa
+    Uczen dziedziczy wszystko? po klasie Osoba
+     */
+    private int nrEwidencyjny;
+
+    public Uczen(String imie, String nazwisko, int wiek) {
+        super(imie, nazwisko, wiek);//wywołujemy konstruktor klasy bazowej
+    }
+
+    public Uczen(String imie, String nazwisko, int wiek,int nrEwidencyjny) {
+        super(imie, nazwisko,wiek);
+        this.nrEwidencyjny = nrEwidencyjny;
+    }
+}
